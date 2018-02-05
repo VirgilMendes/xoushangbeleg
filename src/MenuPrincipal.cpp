90ef61ea4d4694af78ef57243088063607088828
VirgilMendes <virgil.mendes@hotmail.fr>
2018-02-05 05:20:51 +0100

    close #14 Menu terminé

--- src/MenuPrincipal.cpp
+++ src/MenuPrincipal.cpp
@@ -1,15 +1,127 @@
-#include "SFML\Graphics.hpp"
-#include "SFML\Config.hpp"
+#pragma once
+#include <string>
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <Windows.h>
 
-#include "MenuPrincipal.h"
+inline int MenuPrincipalRun() {
 
 
-MenuPrincipal::MenuPrincipal()
-{
-		fenetre.create(sf::VideoMode(800, 600), "My window");
-}
+	sf::RenderWindow fenetre(sf::VideoMode(1000, 700), "MenuTest");
+	fenetre.setVerticalSyncEnabled(true);
+	fenetre.setFramerateLimit(12);
 
-int MenuPrincipal::choix()
-{
+	sf::Texture titre;
+	int frame(-1);
 
+	if (!titre.loadFromFile("ressources/sprite/titre.png"))
+	{
+		std::cout << "error img titre" << std::endl;
+	}
+	titre.setSmooth(false);
+	sf::Sprite sprite;
+	sprite.setTexture(titre);
+
+	sf::IntRect animation[11];
+	animation[0] = sf::IntRect(0, 0, 256, 512);
+	animation[1] = sf::IntRect(256, 0, 256, 512);
+	animation[2] = sf::IntRect(512, 0, 256, 512);
+	animation[3] = sf::IntRect(1024, 0, 256, 512);
+	animation[4] = sf::IntRect(2048, 0, 256, 512);
+	animation[5] = sf::IntRect(2048 * 2, 0, 256, 512);
+	animation[6] = sf::IntRect(2048 * 4, 0, 256, 512);
+	animation[7] = sf::IntRect(2048 * 8, 0, 256, 512);
+	animation[8] = sf::IntRect(2048 * 16, 0, 256, 512);
+	animation[9] = sf::IntRect(2048 * 32, 0, 256, 512);
+	animation[10] = sf::IntRect(2048 * 64, 0, 256, 512);
+
+
+	sprite.setPosition(300, 100);
+
+	sf::Font font; // GESTION DE L'ECRITURE
+
+	int choix(0);
+
+	if (!font.loadFromFile("ressources/VCR_OSD_MONO_1.001.ttf"))
+	{
+		std::cout << "error font" << std::endl;
+	}
+
+	sf::Text TabMenu[4];
+	TabMenu[0].setString("Nouveau");
+	TabMenu[1].setString("Rejoindre");
+	TabMenu[2].setString("Charger");
+	TabMenu[3].setString("Quitter");
+
+	for (int i = 0; i < 4; i++)
+	{
+		TabMenu[i].setFont(font);// choix de la police à utiliser
+		TabMenu[i].setCharacterSize(24);// choix de la taille des caractères
+		TabMenu[i].setFillColor(sf::Color::White);
+		TabMenu[i].setPosition(450, i * 50 + 400);
+	}
+
+	boolean toucheLache(true);   //FIN GESTION DE L'ECRITURE
+
+	while (fenetre.isOpen())
+	{
+		// on inspecte tous les évènements de la fenêtre qui ont été émis depuis la précédente itération
+		sf::Event event;
+		while (fenetre.pollEvent(event))
+		{
+			// évènement "fermeture demandée" : on ferme la fenêtre
+			if (event.type == sf::Event::Closed)
+				fenetre.close();
+		}
+		fenetre.clear(sf::Color::Black);
+
+		frame = frame + 1;
+		if (frame == 11)
+		{
+			frame = 0;
+			std::cout << std::endl;
+		}
+		std::cout << frame;
+
+		sprite.setTextureRect(sf::IntRect(0, 256 * frame, 512, 256));
+
+		TabMenu[choix].setFillColor(sf::Color::White);
+
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up) && toucheLache)
+		{
+			if (choix > 0)
+			{
+				choix -= 1;
+			}
+			toucheLache = false;
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down) && toucheLache)
+		{
+			if (choix < 3)
+			{
+				choix += 1;
+			}
+			toucheLache = false;
+		}
+
+		if (event.type == sf::Event::KeyReleased)
+		{
+			toucheLache = true;
+		}
+
+		TabMenu[choix].setFillColor(sf::Color::Yellow);
+		// c'est ici qu'on dessine tout
+		// window.draw(...);
+		for (int i = 0; i<4; i++)
+		{
+			fenetre.draw(TabMenu[i]);
+		}
+
+		fenetre.draw(sprite);
+
+		sprite.setTextureRect(animation[frame]);
+		// fin de la frame courante, affichage de tout ce qu'on a dessiné
+		fenetre.display();
+	}
+	return 0;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,11 @@
 #include "VueCombat.hpp"
+#include "MenuPrincipal.cpp"
 #include <thread>
 
 int main()
 	{
 		std::thread vueCombat(vueCombatRun);
-		
+		std::thread MenuPrincipal(MenuPrincipalRun);
 		
 		vueCombat.join();
 	}
