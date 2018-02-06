b63637cca9ceabbdbdf5dc264751e2dcc7587f6f
AnthonyStede <stedeschrader.anthony@gmail.com>
2018-02-06 16:17:05 -0500

    Ajout de la vue de Nouvelle partie pour des tests references #12

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,13 @@
 #include "VueCombat.hpp"
 #include "MenuPrincipal.cpp"
+#include "NouvellePartie.cpp"
 #include <thread>
 
 int main()
 	{
 		std::thread vueCombat(vueCombatRun);
 		std::thread menuPrincipal(MenuPrincipalRun);
+		std::thread menuPrincipal(NouvellePartieRun);
 		
 		vueCombat.join();
 		menuPrincipal.join();
