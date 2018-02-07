fce314f99c71b109fbb3213805e831ecac09d1dd
Gordrim <gotdrim@gmail.com>
2018-02-07 17:24:41 -0500

    Tentative d'animation d'unité : clock initialisé à null ?
    reference #13

--- src/Unite.hpp
+++ src/Unite.hpp
@@ -6,42 +6,70 @@
 class Unite : public sf::Drawable
 {
 public:
-	Unite() : position_(0, 0), forme_(sf::Vector2f(64, 64))
+	Unite() : position_(0, 0), clock_(new sf::Clock())
 	{
 		image_.loadFromFile("ressources/sprite/Archer_sprite.png");
 		image_.createMaskFromColor(sf::Color::Transparent, 0);
 
 		texture_.loadFromImage(image_);
 
-		forme_.setPosition(position_.x * 64, position_.y * 64);
-		forme_.setTexture(&texture_);
-		forme_.setTextureRect(sf::IntRect(0,0,64,64));
+		idleAnimation_.push_back(sf::IntRect(0, 0, 64, 64));
+		idleAnimation_.push_back(sf::IntRect(64, 0, 64, 64));
+		idleAnimationState_ = &idleAnimation_.begin();
+
+		sprite_ = new sf::Sprite;
+		sprite_->setPosition(position_.x * 64, position_.y * 64);
+		sprite_->setTexture(texture_);
+		sprite_->setTextureRect(idleAnimation_[0]);
+		
 	}
 
-	Unite(sf::Vector2i position) : position_(position), forme_(sf::Vector2f(64, 64))
+	virtual ~Unite()
+	{
+		delete clock_;
+		delete sprite_;
+		delete idleAnimationState_;
+	}
+
+	Unite(sf::Vector2i position) : position_(position)
 	{
 		image_.loadFromFile("ressources/sprite/Archer_sprite.png");
 		image_.createMaskFromColor(sf::Color::Transparent, 0);
 
 		texture_.loadFromImage(image_);
 
-		forme_.setPosition(position_.x * 64, position_.y * 64);
-		forme_.setTexture(&texture_);
-		forme_.setTextureRect(sf::IntRect(0, 0, 64, 64));
+		sprite_ = new sf::Sprite;
+		sprite_->setPosition(position_.x * 64, position_.y * 64);
+		sprite_->setTexture(texture_);
+		sprite_->setTextureRect(sf::IntRect(0, 0, 64, 64));
 	}
 
 	sf::Vector2i getPosition() { return position_; }
 
 
 private:
-
+	
 	void draw(sf::RenderTarget& target, sf::RenderStates states) const override
 	{
-		target.draw(forme_, states);
+		if (clock_ != nullptr && clock_->getElapsedTime().asSeconds() > 1)
+		{
+			idleAnimationState_->operator++();
+			if (*idleAnimationState_ == idleAnimation_.end())
+			{
+				idleAnimationState_->_Compat(idleAnimation_.begin());
+			}
+			sprite_->setTextureRect(**idleAnimationState_);
+			clock_->restart();
+		}
+		target.draw(*sprite_, states);
 	}
 
 	sf::Vector2i position_;
-	sf::RectangleShape forme_;
+	sf::Sprite* sprite_;
 	sf::Image image_;
 	sf::Texture texture_;
+	sf::Clock* clock_;
+
+	std::vector<sf::IntRect> idleAnimation_;
+	std::vector<sf::IntRect>::iterator* idleAnimationState_;
 };
