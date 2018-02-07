4fc2214493a8465bf5b1b27639d49fee20a59e50
Gordrim <gotdrim@gmail.com>
2018-02-07 15:09:19 -0500

    Deplacement relatif curseur + passage sur du 64x64
    reference #13

--- src/Unite.hpp
+++ src/Unite.hpp
@@ -6,34 +6,28 @@
 class Unite : public sf::Drawable
 {
 public:
-	Unite() : position_(0, 0), forme_(sf::Vector2f(32, 32))
+	Unite() : position_(0, 0), forme_(sf::Vector2f(64, 64))
 	{
 		image_.loadFromFile("ressources/sprite/Archer_sprite.png");
 		image_.createMaskFromColor(sf::Color::Transparent, 0);
 
 		texture_.loadFromImage(image_);
 
-		forme_.setPosition(position_.x * 32, position_.y * 32);
+		forme_.setPosition(position_.x * 64, position_.y * 64);
 		forme_.setTexture(&texture_);
-		forme_.setTextureRect(sf::IntRect(0,0,50,50));
+		forme_.setTextureRect(sf::IntRect(0,0,64,64));
 	}
 
-	Unite(sf::Vector2i position) : position_(position), forme_(sf::Vector2f(32, 32))
+	Unite(sf::Vector2i position) : position_(position), forme_(sf::Vector2f(64, 64))
 	{
 		image_.loadFromFile("ressources/sprite/Archer_sprite.png");
 		image_.createMaskFromColor(sf::Color::Transparent, 0);
 
 		texture_.loadFromImage(image_);
 
-		forme_.setPosition(position_.x * 32, position_.y * 32);
+		forme_.setPosition(position_.x * 64, position_.y * 64);
 		forme_.setTexture(&texture_);
-		forme_.setTextureRect(sf::IntRect(0, 0, 50, 50));
-	}
-
-	void deplacementCurseur(sf::Vector2i position)
-	{
-		position_ = position;
-		forme_.setPosition(position.x * 32, position.y * 32);
+		forme_.setTextureRect(sf::IntRect(0, 0, 64, 64));
 	}
 
 	sf::Vector2i getPosition() { return position_; }
