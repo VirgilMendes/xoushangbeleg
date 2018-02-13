c6de890fea65cde696a9bf2a9e46c3bbdbd3f36b
AnthonyStede <stedeschrader.anthony@gmail.com>
2018-02-12 21:32:15 -0500

    Premières ébauches de la vue Rejoindre une Partie references #12

--- src/Vue/RejoindrePartie.h
+++ src/Vue/RejoindrePartie.h
@@ -1,6 +1,39 @@
+#ifndef DEF_REJOINDREPARTIE
+#define DEF_REJOINDREPARTIE
+#include "GameState.h"
+#include <string>
 #pragma once
 
 namespace Vue
 {
 
+	class RejoindrePartie : GameState
+	{
+	public:
+
+		RejoindrePartie(sf::RenderWindow* fenetre);
+		int run() override;
+		std::string RejoindrePartie::getIP();
+
+
+
+	private:
+		int choix;
+
+		int frame;
+
+		sf::Texture titre;
+		sf::Sprite sprite;
+		sf::IntRect animation[11];
+		std::string str;
+		sf::Text titreCrea;
+		sf::Text numIP;
+		std::string chaineNom;
+		sf::Font font;
+		sf::Text TabMenu[3];
+
+		bool toucheLache;
+	};
+
 }
+#endif
