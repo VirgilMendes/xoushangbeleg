46dd786ac4058a4620722d64b54f96e679581a1d
VirgilMendes <virgil.mendes@hotmail.fr>
2018-02-14 23:57:51 +0100

    version non finie des interface personnages

---
+++ src/Vue/InterfaceUnite.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "SFML/Graphics.hpp"
+#pragma once
+
+#include <string>
+#include "../Modele/Unite.h"
+#include "../Modele/Archer.h"
+#include "../Modele/Soldat.h"
+#include "../Modele/Tank.h"
+
+#include "Jauge.h"
+
+namespace Vue
+{
+
+	class InterfaceUnite : public sf::Drawable
+	{
+	public:
+		InterfaceUnite::InterfaceUnite(Modele::Unite unite);
+
+
+
+	private:
+		Modele::Unite unite;
+
+		Jauge barrevie;
+		sf::RectangleShape fond;
+
+		void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
+	};
+
+
+}
