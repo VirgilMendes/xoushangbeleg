c25430367b5e89b847ae6d28aee34789cb59f3a5
Gordrim <gotdrim@gmail.com>
2018-02-18 18:58:12 -0500

    Animation fonctionnel sur Unite
    reference #21

---
+++ Client/src/Vue/Animation.h
@@ -0,0 +1,59 @@
+#pragma once
+#pragma once 
+#include <vector> 
+#include "SFML/Graphics.hpp" 
+#include "SFML/System.hpp" 
+#include <memory> 
+
+namespace Vue
+{
+	class Animation
+	{
+	public:
+		Animation(std::vector<sf::IntRect> frames, int interval) : frames_(frames),
+			interval_(interval)
+		{
+			frameIndex_ = new int(0);
+			horloge_ = new sf::Clock;
+		}
+		Animation() : interval_(0)
+		{
+			frameIndex_ = new int(0);
+			horloge_ = new sf::Clock();
+		}
+
+		void initialiser(const std::vector<sf::IntRect>& frames, int interval)
+		{
+			frames_ = frames;
+			interval_ = interval;
+		}
+
+		~Animation()
+		{
+			//delete horloge_;
+			//delete frameIndex_;
+		}
+
+		sf::IntRect getFrame() const
+		{
+			
+			
+			if (horloge_->getElapsedTime().asMilliseconds() > interval_)
+			{
+				std::cout << "a" << std::endl;
+				*frameIndex_ += 1;
+				if (*frameIndex_ == frames_.size())
+					*frameIndex_ = 0;
+				horloge_->restart();
+			}
+			return frames_[*frameIndex_];
+		}
+	private:
+
+		std::vector<sf::IntRect> frames_;
+		sf::Clock* horloge_;
+		int* frameIndex_;
+		int interval_;
+
+	};
+}
--- Client/src/Vue/Unite.h
+++ Client/src/Vue/Unite.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <SFML/Graphics.hpp>
+#include "Animation.h"
 
 namespace Vue
 {
@@ -8,34 +9,53 @@ namespace Vue
 	class Unite : public sf::Drawable
 	{
 	public:
-		Unite(std::string nom, sf::Texture* texture) : position_(0, 0), texture_(texture), nom_(nom)
+		Unite(std::string nom, sf::Texture* texture) : position_(0, 0), texture_(texture), nom_(nom), sprite_(nullptr)
 		{
-			sprite_.setPosition(position_.x * 64, position_.y * 64);
-			sprite_.setTexture(*texture_);
-			sprite_.setTextureRect(sf::IntRect(0, 0, 64, 64));
+			sprite_ = new sf::Sprite();
+			sprite_->setPosition(position_.x * 64, position_.y * 64);
+			sprite_->setTexture(*texture_);
+			sprite_->setTextureRect(sf::IntRect(0, 0, 64, 64));
+			
+			std::vector < sf::IntRect > frames; 
+			frames.push_back(sf::IntRect(0, 0, 64, 64)); 
+			frames.push_back(sf::IntRect(0, 128, 64, 64)); 
+			idleaAnimation_.initialiser(frames, 2000); 
 		}
 
-		Unite(std::string nom, sf::Texture* texture, sf::Vector2i position) : texture_(texture), position_(position), nom_(nom)
+		Unite(std::string nom, sf::Texture* texture, sf::Vector2i position) : texture_(texture), position_(position), nom_(nom), sprite_(nullptr)
 		{
-			sprite_.setPosition(position_.x * 64, position_.y * 64);
-			sprite_.setTexture(*texture_);
-			sprite_.setTextureRect(sf::IntRect(0, 0, 64, 64));
+			sprite_ = new sf::Sprite();
+			sprite_->setPosition(position_.x * 64, position_.y * 64);
+			sprite_->setTexture(*texture_);
+			sprite_->setTextureRect(sf::IntRect(0, 0, 64, 64));
+			
+			std::vector < sf::IntRect > frames; 
+			frames.push_back(sf::IntRect(0, 0, 64, 64)); 
+			frames.push_back(sf::IntRect(0, 128, 64, 64)); 
+			idleaAnimation_.initialiser(frames, 2000); 
+		}
+
+		~Unite()
+		{
+			//delete sprite_;
 		}
 
 		sf::Vector2i getPosition() { return position_; }
-		void setPosition(sf::Vector2i position) { position_ = position; sprite_.setPosition(position_.x * 64, position_.y * 64); }
+		void setPosition(sf::Vector2i position) { position_ = position; sprite_->setPosition(position_.x * 64, position_.y * 64); }
 		std::string getNom() { return nom_; }
 
 	private:
 
 		void draw(sf::RenderTarget& target, sf::RenderStates states) const override
 		{
-			target.draw(sprite_, states);
+			sprite_->setTextureRect(idleaAnimation_.getFrame());
+			target.draw(*sprite_, states);
 		}
 
+		Animation idleaAnimation_; 
 		std::string nom_;
 		sf::Vector2i position_;
-		sf::Sprite sprite_;
+		sf::Sprite* sprite_;
 		sf::Image image_;
 		sf::Texture* texture_;
 	};
