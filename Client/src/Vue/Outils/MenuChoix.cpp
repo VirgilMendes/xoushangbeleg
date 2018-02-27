ac3f0340254aa6bf32b5e9ece0fac7e6581f3fd2
Gordrim <gotdrim@gmail.com>
2018-02-27 17:22:51 -0500

    Refactoring MenuChoix + Update mineur Node
    r #22 #23

--- Client/src/Vue/Outils/MenuChoix.cpp
+++ Client/src/Vue/Outils/MenuChoix.cpp
@@ -2,17 +2,17 @@
 
 #include "MenuChoix.h"
 
-Vue::MenuChoix::MenuChoix(std::vector<std::string> textes, sf::Font police, int tailletexte, sf::Vector2i position): textes_(std::vector<Choix>(textes.size())), police_(police), tailletexte_(tailletexte), position_(position)
+Vue::MenuChoix::MenuChoix(std::vector<std::string> textes, sf::Font police, int tailletexte, sf::Vector2i position): police_(police), tailletexte_(tailletexte)
 {
-	for(int i(0); i < textes.size(); i++)
+	for (int i(0); i < textes.size(); i++)
 	{
 		const std::string texteChoix = textes[i];
 		sf::Text texte(texteChoix, police_, tailletexte_);
-		textes_.push_back(Choix{true, texte});
 		texte.setFillColor(couleurActif_);
+		texte.move(5, tailletexte_ * i);
+		
+		textes_.push_back(new Choix(true, texte));
 	}
 
-
-
+	position_ = position;
 }
-
--- Client/src/Vue/Outils/MenuChoix.h
+++ Client/src/Vue/Outils/MenuChoix.h
@@ -2,31 +2,57 @@
 #include <vector>
 #include <SFML/Graphics/Text.hpp>
 #include <SFML/Graphics/RectangleShape.hpp>
+#include "Node.h"
+#include <iostream>
 
 namespace Vue
 {
 	struct Choix
 	{
+		Choix(){}
+		Choix(bool actif, sf::Text& texte): actif(actif), texte(texte){}
 		bool actif;
 		sf::Text texte;
 	};
 
-	class MenuChoix
+	class MenuChoix : public Node
 	{
 	public:
 
 		MenuChoix(std::vector<std::string> textes, sf::Font police, int tailletexte, sf::Vector2i position = sf::Vector2i(0, 0));
+		~MenuChoix()
+		{
+			for (Choix* choix : textes_)
+			{
+				delete choix;
+			}
+		}
 
 	private:
 		
-		//Gestion de la boite
-		sf::RectangleShape boite_;
-		sf::Vector2i position_;
+		//Methode de Node 
+		void dessinerNode(sf::RenderTarget& target, const sf::Transform& transform) override
+		{
+			for(Choix* choix : textes_)
+			{
+				target.draw(choix->texte, transform);
+			}
+		}
+		virtual void setPositionElements(sf::Vector2i position) override
+		{
+			for (int i(0); i < textes_.size(); i++)
+			{
+				textes_[i]->texte.setPosition(position.x, position.y);
+				textes_[i]->texte.move(5, tailletexte_ * i);
+			}
+		}
+		virtual void setTailleElements(sf::Vector2i taille) override{}
+		virtual void setRotationElements(int rotation) override{}
 		
 		//Gestion des textes
 		sf::Font police_;
 		int tailletexte_;
-		std::vector<Choix> textes_;
+		std::vector<Choix*> textes_;
 
 		//Gestion des couleurs
 		sf::Color couleurSelection_ = sf::Color::Yellow;
