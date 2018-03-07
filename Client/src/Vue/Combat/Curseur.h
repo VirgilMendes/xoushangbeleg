34373f55c8cfa6b7116ba0f790f4e3be233236f3
VirgilMendes <virgil.mendes@hotmail.fr>
2018-03-07 21:46:49 +0100

    animation curseur terminée

--- Client/src/Vue/Combat/Curseur.h
+++ Client/src/Vue/Combat/Curseur.h
@@ -1,5 +1,7 @@
 #pragma once
 #include <SFML/Graphics.hpp>
+#include "../Outils/Animation.h"
+#include <iostream>
 
 namespace Vue
 {
@@ -7,37 +9,63 @@ namespace Vue
 	class Curseur : public sf::Drawable
 	{
 	public:
-		Curseur() : position_(0, 0), forme_(sf::Vector2f(64, 64))
+		Curseur() : position_(0, 0), sprite_(nullptr), texture_(nullptr)
 		{
-			image_.create(64, 64, sf::Color::Red);
-			image_.createMaskFromColor(sf::Color::Red, 25);
+			sprite_ = new sf::Sprite();
+			texture_ = new sf::Texture();
+			if (!texture_->loadFromFile("ressources/sprite/AnimCurseur.png"))
+			{
+				std::cout << "erreur chargement Texture AnimCurseur.png" << std::endl;
+			}
 
-			texture_.loadFromImage(image_);
-
-			forme_.setPosition(position_.x * 64, position_.y * 64);
-			forme_.setTexture(&texture_);
+			sprite_->setPosition(position_.x * 64, position_.y * 64);
+			sprite_->setTexture(*texture_);
+			sprite_->setTextureRect(sf::IntRect(0, 0, 64, 64));
+			initAnimation();
 		}
 
-		Curseur(sf::Vector2i position) : position_(position), forme_(sf::Vector2f(64, 64))
+		Curseur(sf::Vector2i position) : position_(position), sprite_(nullptr),texture_(nullptr)
 		{
-			image_.create(64, 64, sf::Color::Red);
-			image_.createMaskFromColor(sf::Color::Red, 25);
+			sprite_ = new sf::Sprite();
+			texture_ = new sf::Texture();
+			if(!texture_->loadFromFile("ressources/sprite/AnimCurseur.png"))
+			{
+				std::cout << "erreur chargement Texture AnimCurseur.png" << std::endl;
+			}
 
-			texture_.loadFromImage(image_);
+			sprite_->setPosition(position_.x * 64, position_.y * 64);
+			sprite_->setTexture(*texture_);
+			sprite_->setTextureRect(sf::IntRect(0, 0, 64, 64));
+			initAnimation();
+		}
 
-			forme_.setPosition(position_.x * 64, position_.y * 64);
-			forme_.setTexture(&texture_);
+
+		void initAnimation()
+		{
+			const std::vector < sf::IntRect > stay =
+			{
+				sf::IntRect(0, 0, 64, 64),
+				sf::IntRect(64, 0, 64, 64),
+				sf::IntRect(128, 0, 64, 64),
+				sf::IntRect(192, 0, 64, 64),
+				sf::IntRect(256, 0, 64, 64),
+				sf::IntRect(320, 0, 64, 64),
+				sf::IntRect(384, 0, 64, 64),
+				sf::IntRect(448, 0, 64, 64)
+			};
+			stay_.initialiser(stay, 100);
+
 		}
 
 		void deplacerCurseur(sf::Vector2i positionRelative)
 		{
 			position_ += positionRelative;
-			forme_.setPosition(position_.x * 64, position_.y * 64);
+			sprite_->setPosition(position_.x * 64, position_.y * 64);
 		}
 		void setPosition(sf::Vector2i position)
 		{
 			position_ = position;
-			forme_.setPosition(position.x * 64, position.y * 32);
+			sprite_->setPosition(position.x * 64, position.y * 64);
 		}
 
 		sf::Vector2i getPosition() { return position_; }
@@ -47,12 +75,14 @@ namespace Vue
 
 		void draw(sf::RenderTarget& target, sf::RenderStates states) const override
 		{
-			target.draw(forme_, states);
+			sprite_->setTextureRect(stay_.getFrame());
+			target.draw(*sprite_, states);
 		}
 
+		Animation stay_;
+
 		sf::Vector2i position_;
-		sf::RectangleShape forme_;
-		sf::Image image_;
-		sf::Texture texture_;
+		sf::Texture* texture_;
+		sf::Sprite* sprite_;
 	};
 }
